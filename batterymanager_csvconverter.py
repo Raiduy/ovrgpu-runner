ba253a6a909ba9311f7104515fe2fe0cf4a15ece
Radu Apsan <radu.apsan@gmail.com>
2023-12-01 00:42:12 +0100

    todo: finalize metrics getter

--- batterymanager_csvconverter.py
+++ batterymanager_csvconverter.py
@@ -1,21 +1,50 @@
 import pandas as pd
 import numpy as np
+import re
 import os
 
 RAW_DATA = './raw_data/batterymanager/batterymanager-companion.log'
+HEADER_PATTERN = 'BatteryMgr:DataCollectionService: onStartCommand: rawFields => '
+DATA_PATTERN = 'BatteryMgr:DataCollectionService: stats => '
+
+def preprocess_values(df):
+    df['Timestamp'] = df['Timestamp'] - df['Timestamp'][0]
+    # conversion from microseconds to seconds
+    df['Timestamp'] = df['Timestamp'] / 1000
+    return df
+
+def calculate_power(df):
+    df['power'] = (abs(df['BATTERY_PROPERTY_CURRENT_NOW']) / 1000 / 1000) * (df['EXTRA_VOLTAGE'] / 1000)
+    return df
+
+def trapezoid_method(df):
+    return np.trapz(df['power'].values, df['Timestamp'].values)
 
 
+def get_column_names(file):
+    pattern = re.compile(HEADER_PATTERN)
+    cols = []
+    for line in open(file):
+        if pattern.search(line):
+            cols = line.split(HEADER_PATTERN)[1].split(',')
+            cols = [col.strip() for col in cols]
+            break
+    return cols
+
+def get_data(file):
+    pattern = re.compile(DATA_PATTERN)
+    data = []
+    for line in open(file):
+        if pattern.search(line):
+            data.append(line.split(DATA_PATTERN)[1].strip('\n').split(','))
+
+    return data
 
 def main():
-    for line in open(RAW_DATA):
-        if 'rawFields => ' in line:
-            cols = line.split('rawFields => ')[1].split(',')
-            cols = [col.strip() for col in cols]
-            
-            print(cols)
-
-
-
+    cols = get_column_names(RAW_DATA)
+    data = get_data(RAW_DATA)
+    df = pd.DataFrame(data, columns=cols)
+    df.to_csv('./data/batterymanager/batterymanager.csv', index=False)
 
 if __name__ == '__main__':
     main()
